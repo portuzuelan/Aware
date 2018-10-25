dbce86d74ab945d2dac8cd128f3ba63c67f5adf9
Antonio Omar Pereira <aomarpereira@antonios-mbp.attlocal.net>
2018-10-25 14:30:05 -0400

    aware

---
+++ companySite.js
@@ -0,0 +1,19 @@
+alert ("This site is still under construction.");
+
+
+
+
+//While loop
+var count = 1;
+
+while (count <= 10) {
+  console.log("The count is " + count);
+  count++;
+}
+
+//For loop
+//(its the same as the above, but in different syntax)
+
+
+var button1 = document.querySelector(.button1);
+button1.style.color = "pink";
